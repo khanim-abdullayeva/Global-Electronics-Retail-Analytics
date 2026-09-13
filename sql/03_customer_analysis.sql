5607d7798966ea8847e7c3c0f6cb1434b2eefa31
Khanim Abdullayeva <xabdullayeva775@gmail.com>
2026-09-14 01:08:37 +0400

    Add Customer Analysis SQL File

--- sql/03_customer_analysis.sql
+++ sql/03_customer_analysis.sql
@@ -22,4 +22,45 @@ LIMIT 10;
 -- Top 10 customers who had the highest number of orders
 
 
+
+
 SELECT 
+	o.*,
+	s."Product Name",
+	s.Category,
+	c.Name,
+	c.Gender,
+	c.Country
+FROM 
+	(SELECT 
+		CustomerKey,
+		COUNT("Order Number") AS " Number of Orders",
+		SUM(Quantity * "Gross Profit USD") AS "Total Revenue"
+	FROM 
+		sales_analysis
+	GROUP BY
+		CustomerKey
+	ORDER BY
+		"Total Revenue" DESC) o
+LEFT JOIN
+	customers c
+ON o.CustomerKey = c.CustomerKey
+LEFT JOIN sales_analysis s
+ON c.CustomerKey = s.CustomerKey
+LIMIT 10;
+	
+-- Top 10 customers with the highest total revenue
+
+
+
+SELECT 
+	CustomerKey,
+	ROUND(SUM(Quantity * "Unit price USD")/COUNT(DISTINCT CustomerKey),2) as "Average Revenue per Customer"
+FROM 
+	sales_analysis
+ORDER BY
+	"Average Revenue per Customer";
+	
+-- Average revenue per customer
+
+

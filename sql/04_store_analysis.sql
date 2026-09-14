677c4e25cffffbfc6511618cf9ec45faa40db788
Khanim Abdullayeva <xabdullayeva775@gmail.com>
2026-09-14 16:15:26 +0400

    Add Store Analysis SQL File

---
+++ sql/04_store_analysis.sql
@@ -0,0 +1,42 @@
+SELECT 
+	sa.*,
+	st.Country
+FROM
+	(SELECT 
+		StoreKey,
+		SUM(Quantity * "Unit price USD") AS "Total Revenue"
+	FROM
+		sales_analysis 
+	GROUP BY 
+		"StoreKey") sa
+LEFT JOIN
+	stores st
+ON
+	sa.StoreKey = st.StoreKey
+ORDER BY
+	"Total Revenue" DESC;
+	
+-- Total Revenue by Store
+
+
+SELECT 
+	sa.*,
+	st.Country
+FROM
+	(SELECT
+		StoreKey,
+		SUM(Quantity * "Gross Profit USD") AS "Total Profit"
+	FROM
+		sales_analysis 
+	GROUP BY 
+		"StoreKey") sa
+LEFT JOIN
+	stores st
+ON
+	sa.StoreKey = st.StoreKey
+ORDER BY
+	"Total Profit" DESC;
+	
+-- Total Profit by Store
+
+
